2af97f3e045a7759f60fcfa60367c68a22312c44
CameronWD <14041@coderacademy.edu.au>
2023-05-17 16:51:19 +1000

    completed bash script for help file

---
+++ dropcalculator.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if [[ -x "$(command -v python3)" ]]
+then
+    pyv="$(python3 -V 2>&1)"
+    if [[ $pyv == "Python 3.8"* ]] || [[ $pyv == "Python 3.9"* ]] || [[ $pyv == "Python 3.10"* ]]
+    then
+        python3 src/main.py
+    else
+        echo "Your python version is too old. Please update Python and try again." >&2
+        exit 1
+    fi 
+else
+    echo "You don't have python installed. Please go to https://www.python.org to download it." >&2
+    exit 1
+fi
+
+python3 -m venv venv
+
+source venv/bin/activate
+
+pip3 install --upgrade pip
+
+pip3 install -r requirments.txt
+
+python3 src/main.py
+
+deactivate
